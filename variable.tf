4152cea541aa5d032418feaefbadceacf0fe8e8a
Dhirendra kushwaha <kushwahadk799@gmail.com>
2026-08-27 00:11:38 +0530

    delete 1

--- variable.tf
+++ variable.tf
@@ -1,13 +1,1 @@
-variable "resource_group" {}
 
-variable "virtual_network" {}
-
-variable "subnet" {}
-
-variable "network_security_group" {}
-
-variable "nic" {}
-
-variable "pip" {}
-
-variable "vm" {}
